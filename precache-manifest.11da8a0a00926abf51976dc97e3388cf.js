4b729def3cd19fdd2fd4c4ed5e9e43ab03dfaf69
Sahak1994 <sahak.keshishyan@mail.ru>
2020-03-06 17:09:25 +0400

    Updates

---
+++ precache-manifest.11da8a0a00926abf51976dc97e3388cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32715be5b03f22ecc38facba5d7cc01b",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "8750171369700b889b58",
+    "url": "/re-store/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "21e7cf85be33de8c1562",
+    "url": "/re-store/static/js/2.d570d17a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/re-store/static/js/2.d570d17a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8750171369700b889b58",
+    "url": "/re-store/static/js/main.e3414f63.chunk.js"
+  },
+  {
+    "revision": "24a65a360198fd44398f",
+    "url": "/re-store/static/js/runtime-main.cc92e0fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/re-store/static/media/logo.5d5d9eef.svg"
+  }
+]);
