9d096ab46b300674d0ac307bcea4bc900d7aa6f3
Sahak1994 <sahak.keshishyan@mail.ru>
2020-04-11 23:48:21 +0400

    Updates

---
+++ precache-manifest.52f43581aa3e331477652022b6f89192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860b57a186484969225175f8bd5df7e8",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "e625cae1a05620e0867c",
+    "url": "/re-store/static/css/main.7ce9c276.chunk.css"
+  },
+  {
+    "revision": "941c65116df3083927db",
+    "url": "/re-store/static/js/2.09843764.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/re-store/static/js/2.09843764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e625cae1a05620e0867c",
+    "url": "/re-store/static/js/main.4821f195.chunk.js"
+  },
+  {
+    "revision": "24a65a360198fd44398f",
+    "url": "/re-store/static/js/runtime-main.cc92e0fe.js"
+  }
+]);
